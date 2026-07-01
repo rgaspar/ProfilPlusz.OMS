c6fb09e8764df8edf510966a17ebc76ae528207d
rgaspar <rgaspar@sdlc-it.com>
2026-07-01 14:59:27 +0200

    migration: add ContactPersonName column to Customer table

---
+++ Infrastructure/Infrastructure/Migrations/20260701125436_AddCustomerAddresses.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerAddresses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPersonName",
+                table: "Customer",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactPersonName",
+                table: "Customer");
+        }
+    }
+}
